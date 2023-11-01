dad052f3f6add558ed3c1d5b15359a5a4e76c73f
Fraz Ali <frazaliqureshi@gmail.com>
2023-11-01 18:51:36 +0100

    solution: The grandest staricase of them all

--- the-grandest-staircase-of-them-all/solution.py
+++ the-grandest-staircase-of-them-all/solution.py
@@ -1,10 +1,22 @@
 def solution(n):
-    print("Stairs: = ", n)
+    stairs = [None]*(n+1)
+    for i in range(n+1):
+        stairs[i] = [None]*(n+1)
+        for j in range(n+1):
+            stairs[i][j] = 0
+
+    stairs[0][0] = 1
+    for i in range(1, n+1):
+        for j in range(n+1):
+            stairs[i][j] = stairs[i-1][j]
+            if j >= i:
+                stairs[i][j] += stairs[i-1][j-i]
+    return stairs[n][n] - 1
 
 
 if __name__ == "__main__":
-    solution(3)
-    solution(4)
-    solution(5)
-    solution(200)
+    print(solution(3))
+    print(solution(4))
+    print(solution(5))
+    print(solution(200))
 
